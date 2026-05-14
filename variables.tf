2ed0017df873ef86f04c12f2a14c6cb0428bdf26
sreeni_github <sreenigmfs@gmail.com>
2026-05-14 12:50:40 +0100

    variable and main files are changed for testing

--- variables.tf
+++ variables.tf
@@ -9,3 +9,14 @@ variable "env_file" {
     }
     sensitive = false
 }  
+
+variable "source_file" {
+    type = string
+    default = "devfile.txt"
+    description = "it's an environment file"
+    validation {
+        condition = true
+        error_message = "Error message if validation fails"
+    }
+    sensitive = false
+}  
